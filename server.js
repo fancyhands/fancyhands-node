a68e7bc483c71f346e591e4e7d9b066c67c0edb3
Orry Baram <orrybaram@gmail.com>
2014-08-06 17:55:37 -0400

    things works

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
-var FH = require('fancyhand-node')
+var FH = require('./lib/fancyhands').fancyhands
 
-console.log(FH)
+FH.config('PuREN1kznQ4UyWI', 'dzvNP3hg0idkb0x')
+
+FH.post('/api/v1/echo/', {'hahaha': 'hahahahaha'}, function(err, data, response) {
+	console.log(err)
+	console.log(data)
+});
