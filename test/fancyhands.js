1a20e97f9116496a04ff7ec527eba8f174d1ba73
Orry Baram <orrybaram@gmail.com>
2014-08-07 13:21:11 -0400

    changes made

--- test/fancyhands.js
+++ test/fancyhands.js
@@ -2,17 +2,19 @@ var should = require('chai').should();
 var fancyhands = require('../index').fancyhands;
 var test_data = require('../test/test-data');
 
+
+// Change to your key, and secret
 fancyhands.config('PuREN1kznQ4UyWI', 'dzvNP3hg0idkb0x')
 
 describe('fancyhands', function() {
-	// describe('#echo()', function() {
-	// 	it('Should echo something :) ', function(done) {
-	// 		fancyhands.echo({'test': 'test'}, function(data) {
-	// 			JSON.parse(data).should.have.property('test')
-	// 			done();
-	// 		})
-	// 	})
-	// });
+	describe('#echo()', function() {
+		it('Should echo something :) ', function(done) {
+			fancyhands.echo({'test': 'test'}, function(data) {
+				JSON.parse(data).should.have.property('test')
+				done();
+			})
+		})
+	});
 
 	describe('#create_call()', function() {
 		it('Should create a call and return request data', function(done) {
