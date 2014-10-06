a9596b99efb794012ca76e989efa431f44cf8edb
Orry Baram <orrybaram@gmail.com>
2014-10-06 10:29:20 -0400

    Update example.js

--- example.js
+++ example.js
@@ -2,6 +2,6 @@ var FH = require('./lib/fancyhands').fancyhands
 
 FH.config('YOUR_KEY', 'YOUR_SECRET')
 
-FH.echo({'hello': 'world'}, function(data) {
+FH.echo({'hello': 'world'}).then(function(data) {
 	console.log(data)
 })
